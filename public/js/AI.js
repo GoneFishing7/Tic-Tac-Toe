ed478b215eafc083896b60a5aa1281af72801b15
Joshua Imoo <imoojoshua@gmail.com>
2020-05-21 17:15:02 -0700

    add hard mode

--- public/js/AI.js
+++ public/js/AI.js
@@ -16,9 +16,77 @@ var AI = function () {
                 return this.findBestMoveFromList(board);
             } else if (mode === "medium") {
                 return this.findRandomMove(board);
+            } else if (mode === "hard") {
+                return this.findLimitedMinimaxMove(board);
             } else {
                 return this.findBestMinimaxMove(board);
             }
+        }
+    }, {
+        key: "findLimitedMinimaxMove",
+        value: function findLimitedMinimaxMove(board) {
+            var possibleMoves = board.getBlankSquares();
+            var bestMoves = [];
+            var bestMoveEval = -2;
+            for (var moveIndex = 0; moveIndex < possibleMoves.length; moveIndex++) {
+                var move = possibleMoves[moveIndex];
+                var boardWithNewMove = board.copyWithMove(symbols["computer"], move.row, move.cell);
+                var minimaxEval = this.evalMoveLimitedMinimax(boardWithNewMove, -1, 0);
+                if (minimaxEval > bestMoveEval) {
+                    bestMoveEval = minimaxEval;
+                    bestMoves = [move];
+                } else if (minimaxEval == bestMoveEval) {
+                    bestMoves.push(move);
+                }
+            }
+            if (bestMoveEval == -2) {
+                console.log("Something bad happened!");
+                return null;
+            }
+            var bestMove = Utils.randomElementFromArray(bestMoves);
+            return bestMove;
+        }
+    }, {
+        key: "evalMoveLimitedMinimax",
+        value: function evalMoveLimitedMinimax(board, turn, depth) {
+            var MAX_DEPTH = 5; // 1 "Depth" is a move one way, 2 "depths" is one move from each side
+            if (depth >= MAX_DEPTH) {
+                return turn;
+            }
+            var possibleMoves = board.getBlankSquares();
+            // Check for terminal states
+            var gameState = board.getGameState();
+            if (gameState !== symbols["blank"]) {
+                return gameState == symbols["computer"] ? 1 : -1;
+            }
+            if (possibleMoves.length < 1) {
+                return 0;
+            }
+            var bestMoveEval = 0;
+            if (turn == -1) {
+                // MINI
+                bestMoveEval = 2;
+                for (var i = 0; i < possibleMoves.length; i++) {
+                    var move = possibleMoves[i];
+                    var boardWithNewMove = board.copyWithMove(symbols["player"], move.row, move.cell);
+                    var currentMoveEval = this.evalMoveLimitedMinimax(boardWithNewMove, -turn, depth + 1);
+                    if (currentMoveEval < bestMoveEval) {
+                        bestMoveEval = currentMoveEval;
+                    }
+                }
+            } else {
+                // MAX
+                bestMoveEval = -2;
+                for (var _i = 0; _i < possibleMoves.length; _i++) {
+                    var _move = possibleMoves[_i];
+                    var _boardWithNewMove = board.copyWithMove(symbols["computer"], _move.row, _move.cell);
+                    var _currentMoveEval = this.evalMoveLimitedMinimax(_boardWithNewMove, -turn, depth + 1);
+                    if (_currentMoveEval > bestMoveEval) {
+                        bestMoveEval = _currentMoveEval;
+                    }
+                }
+            }
+            return bestMoveEval;
         }
     }, {
         key: "findBestMinimaxMove",
@@ -71,12 +139,12 @@ var AI = function () {
             } else {
                 // MAX
                 bestMoveEval = -2;
-                for (var _i = 0; _i < possibleMoves.length; _i++) {
-                    var _move = possibleMoves[_i];
-                    var _boardWithNewMove = board.copyWithMove(symbols["computer"], _move.row, _move.cell);
-                    var _currentMoveEval = this.evalMoveMinimax(_boardWithNewMove, -turn);
-                    if (_currentMoveEval > bestMoveEval) {
-                        bestMoveEval = _currentMoveEval;
+                for (var _i2 = 0; _i2 < possibleMoves.length; _i2++) {
+                    var _move2 = possibleMoves[_i2];
+                    var _boardWithNewMove2 = board.copyWithMove(symbols["computer"], _move2.row, _move2.cell);
+                    var _currentMoveEval2 = this.evalMoveMinimax(_boardWithNewMove2, -turn);
+                    if (_currentMoveEval2 > bestMoveEval) {
+                        bestMoveEval = _currentMoveEval2;
                     }
                 }
             }
--- src/js/AI.js
+++ src/js/AI.js
@@ -4,9 +4,75 @@ class AI {
             return this.findBestMoveFromList(board);
         } else if (mode === "medium") {
             return this.findRandomMove(board);
+        } else if (mode === "hard") {
+            return this.findLimitedMinimaxMove(board);
         } else { 
             return this.findBestMinimaxMove(board);
         }
+    }
+
+    static findLimitedMinimaxMove(board) {
+        let possibleMoves = board.getBlankSquares();
+        let bestMoves = [];
+        let bestMoveEval = -2;
+        for (let moveIndex = 0; moveIndex < possibleMoves.length; moveIndex++) {
+            let move = possibleMoves[moveIndex]
+            let boardWithNewMove = board.copyWithMove(symbols["computer"], move.row, move.cell);
+            let minimaxEval = this.evalMoveLimitedMinimax(boardWithNewMove, -1, 0);
+            if (minimaxEval > bestMoveEval) {
+                bestMoveEval = minimaxEval;
+                bestMoves = [move];
+            } else if (minimaxEval == bestMoveEval) {
+                bestMoves.push(move);
+            }
+        }
+        if (bestMoveEval == -2) {
+            console.log("Something bad happened!");
+            return null;
+        }
+        let bestMove = Utils.randomElementFromArray(bestMoves);
+        return bestMove;
+    }
+
+    static evalMoveLimitedMinimax(board, turn, depth) {
+        const MAX_DEPTH = 5; // 1 "Depth" is a move one way, 2 "depths" is one move from each side
+        if (depth >= MAX_DEPTH) {
+            return turn;
+        }
+        let possibleMoves = board.getBlankSquares();
+        // Check for terminal states
+        let gameState = board.getGameState();
+        if (gameState !== symbols["blank"]) {
+            return gameState == symbols["computer"] ? 1 : -1;
+        }
+        if (possibleMoves.length < 1) {
+            return 0;
+        }
+        let bestMoveEval = 0;
+        if (turn == -1) {
+            // MINI
+            bestMoveEval = 2;
+            for (let i = 0; i < possibleMoves.length; i++) {
+                let move = possibleMoves[i];
+                let boardWithNewMove = board.copyWithMove(symbols["player"], move.row, move.cell);
+                let currentMoveEval = this.evalMoveLimitedMinimax(boardWithNewMove, -turn, depth+1);
+                if (currentMoveEval < bestMoveEval) {
+                    bestMoveEval = currentMoveEval;
+                }
+            }
+        } else {
+            // MAX
+            bestMoveEval = -2;
+            for (let i = 0; i < possibleMoves.length; i++) {
+                let move = possibleMoves[i];
+                let boardWithNewMove = board.copyWithMove(symbols["computer"], move.row, move.cell);
+                let currentMoveEval = this.evalMoveLimitedMinimax(boardWithNewMove, -turn, depth+1);
+                if (currentMoveEval > bestMoveEval) {
+                    bestMoveEval = currentMoveEval;
+                }
+            }
+        }
+        return bestMoveEval;
     }
 
     static findBestMinimaxMove(board) {
